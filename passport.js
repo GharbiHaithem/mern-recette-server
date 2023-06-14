5b496681b8f05a29254fd987e519460e9989c5d9
GharbiHaithem <gharbi.haythem1988@gmail.com>
2023-06-14 14:31:23 +0200

    deploy server

--- passport.js
+++ passport.js
@@ -15,7 +15,7 @@ const passport = require('passport')
 passport.use(new GoogleStrategy({
     clientID: "483148789252-3u00lc95p6ct96nh1hedoc2l0d392aja.apps.googleusercontent.com",
     clientSecret: "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",
-    callbackURL: `${process.env.BASE_URL}/auth/google/callback`,
+    callbackURL: `https://mern-recette-server.vercel.app/auth/google/callback`,
     scope: ["profile", "email"],
     userProfileURL:"https://www.googleapis.com/oauth2/v3/userinfo"
 
