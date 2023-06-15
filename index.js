94b4c7ddb58de7d18e9b30e88cb99c5f405a0dfe
GharbiHaithem <gharbi.haythem1988@gmail.com>
2023-06-15 14:20:26 +0200

    deploy final

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.use(passport.initialize())
 
 app.use(passport.session())
 app.use(cors({
-    origin:"http://localhost:3000",
+    origin:"https://heroic-banoffee-4688af.netlify.app",
     methods:"GET,POST,PUT,DELETE",
     credentials:true
 }))
