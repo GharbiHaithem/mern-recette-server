924a98aadefaa6817ce71f601447a3eabe6f4c15
GharbiHaithem <gharbi.haythem1988@gmail.com>
2023-06-15 15:46:57 +0200

    deploy project

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.use(passport.initialize())
 
 app.use(passport.session())
 app.use(cors({
-    origin:"https://mern-recette-client.vercel.app",
+    origin:"https://recette-beta.vercel.app",
     methods:"GET,POST,PUT,DELETE",
     credentials:true
 }))
