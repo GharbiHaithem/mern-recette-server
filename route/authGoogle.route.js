7c6c01165fe68a89b944b86d0357b87d81b722ec
GharbiHaithem <gharbi.haythem1988@gmail.com>
2023-06-20 09:47:24 +0200

    V5

--- route/authGoogle.route.js
+++ route/authGoogle.route.js
@@ -19,7 +19,7 @@ router.get("/auth/login/failed",(req,res)=>{
 })
 router.get("/auth/google",passport.authenticate("google",["profile","email"]))
 router.get("/auth/google/callback",passport.authenticate("google",{
-    successRedirect:"https://recette-mern-projects.vercel.app//myrecette",
+    successRedirect:"https://recette-mern-projects.vercel.app/myrecette",
     failureRedirect:"/login/failed"
 }))
 router.get("/auth/logout",async(req,res)=>{
@@ -30,7 +30,7 @@ router.get("/auth/logout",async(req,res)=>{
         }
         // Effectuer d'autres actions après la déconnexion réussie
         // Rediriger l'utilisateur vers une autre page, etc.
-        res.redirect("https://recette-mern-projects.vercel.app/")
+        res.redirect("https://recette-mern-projects.vercel.app")
       });
   
 })
